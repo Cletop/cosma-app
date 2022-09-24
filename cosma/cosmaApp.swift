5321de00090ff525c73ae70773cb5a54f13907ae
Branlice <lishiwen@wclound.cn>
2022-09-24 18:02:00 +0800

    Initial Commit

---
+++ cosma/cosmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cosmaApp.swift
+//  cosma
+//
+//  Created by branilce on 2022/9/24.
+//
+
+import SwiftUI
+
+@main
+struct cosmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
