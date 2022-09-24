948ca208c2d9c7371603c4b7d8c52a2b982fe5d3
Branlice <lishiwen@wclound.cn>
2022-09-24 18:22:19 +0800

    chore: init cosma switf app

--- cosma/ContentView.swift
+++ cosma/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Text("Swift App")
+                .font(.title)
+                .padding(6.0)
+                
             Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Button {            } label: {
+                Text("Start Now")
+                    .padding(20)
+                    .background(Color.accentColor)
+                    .foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
